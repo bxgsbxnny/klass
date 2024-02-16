0f776ecbbc3288e8f459196c9cc8f5c7ac586d3f
killpenis <158824846+bxgsbxnny@users.noreply.github.com>
2024-02-16 14:19:01 +0300

    16.02

---
+++ 16.02.py
@@ -0,0 +1,41 @@
+#задача 1
+#a=int(input())
+#b=int(input())
+#if a>b:
+#    print(a)
+#elif b>a:
+#    print(b)
+#else:
+#   print("числа равны")
+
+#задача 2
+#a=int(input("сторона квадрата"))
+#b=int(input("радиус круга"))
+#if b<=a/2:
+#    print("круг впишется")
+#else:
+#    print("не впишется")
+
+#задача 3
+#a=int(input("x"))
+#b=a**2
+#if b<0:
+#    print("y <= 0")
+#else:
+#    print("y >= 0")
+
+#задача 4
+#a=int(input("сторона квадрата"))
+#b=int(input("радиус круга"))
+#if a<=b*2:
+#    print("квадрат впишется")
+#else:
+#    print("не впишется")
+
+#задача 5
+#a=int(input())
+#b=int(input())
+#if a>b:
+#    print(a)
+#else:
+#    print(b)
